6a5f1fb8cca9c928f41a469bb89d29122d7b1493
sisakov82 <sisakov82@gmail.com>
2021-05-16 18:04:00 +0300

    AddFindServiceStaticTokenClient

--- FindService.Client/Configuration/FindServiceClientConfiguration.cs
+++ FindService.Client/Configuration/FindServiceClientConfiguration.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Refit;
 using System;
 using System.Net.Http;
-using TextService.Client;
 
 namespace FindService.Client.Configuration
 {
@@ -17,18 +16,33 @@ namespace FindService.Client.Configuration
                 new HttpClient(
                 new HttpClientHandler { ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true })
                 {
-                    BaseAddress = new Uri(configuration["ServiceUrls:FindService"]),                    
-                    Timeout = TimeSpan.FromMinutes(5)
+                    BaseAddress = new Uri(configuration["ServiceUrls:FindService"]),
                 }));
-            
+
             return services;
         }
-
         public static IServiceCollection AddFindServiceTokenClient(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddApiClient<IFindClient>(configuration, new RefitSettings(), "ServiceUrls:FindService");
 
             return services;
         }
+        public static IServiceCollection AddFindServiceStaticTokenClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApiClientBGWorker<IFindClient>(configuration, new RefitSettings(), "ServiceUrls:FindService");
+
+            return services;
+        }
+
+        //public static IServiceCollection AddFindServiceGetTokenClient(this IServiceCollection services, IConfiguration configuration)
+        //{
+        //    var refitSettings = new RefitSettings
+        //    {
+        //        AuthorizationHeaderValueGetter = () => new AuthService().GetToken(new LoginModel { Username = "QWE", Password = "QweAsd123!" })
+        //    };
+        //    services.AddApiClient<IFindClient>(configuration, refitSettings, "ServiceUrls:FindService");
+
+        //    return services;
+        //}
     }
 }
